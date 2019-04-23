b054c8f16238c04002f27a9dd30d7944eeb32dae
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-04-23 22:49:45 +0530

    Added java language fundamental project

---
+++ ProgrammingLanguages/Java/LanguageFundamentals/src/main/java/org/redquark/java/fundamentals/introduction/Introduction01.java
@@ -0,0 +1,22 @@
+package org.redquark.java.fundamentals.introduction;
+
+/**
+ * @Author Anirudh Sharma
+ * <p>
+ * This class prints the introductory message of "Hello World!" to the console.
+ */
+public class Introduction01 {
+
+	/**
+	 * main() method is the entry point of any java program i.e. a java program starts its execution from the main
+	 * method
+	 *
+	 * @param args - Array which contains the arguments that are passed while running the program.
+	 *             This is normally done while running a java program via command line
+	 */
+	public static void main(String[] args) {
+
+		// Printing a message to the console
+		System.out.println("Hello World!");
+	}
+}

8d4b9196fab033ae9cd5cd92cd54ab715f175b67
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-04-25 22:52:31 +0530

    Added code for types of constructors

---
+++ ProgrammingLanguages/Java/LanguageFundamentals/src/main/java/org/redquark/java/fundamentals/introduction/Introduction3.java
@@ -0,0 +1,78 @@
+package org.redquark.java.fundamentals.introduction;
+
+/**
+ * @Author Anirudh Sharma
+ * <p>
+ * This class shows the usage of constructor in Java
+ */
+public class Introduction3 {
+
+	// Class variable
+	private String name;
+
+	/*
+	 * There are three types of constructor in java.
+	 * 1. Default Constructor
+	 * 2. No-Args constructor
+	 * 3. Parameterized constructor
+	 */
+
+	/*
+	 * 1. Default constructor
+	 * -----------------------
+	 * It’s not required to always provide a constructor implementation in the class code.
+	 * If we don’t provide a constructor, then java provides default constructor implementation for us to use.
+	 *
+	 * Default constructor only role is to initialize the object and return it to the calling code.
+	 *
+	 * Default constructor is always without argument and provided by java compiler only when there is no existing
+	 * constructor defined.
+	 *
+	 * Most of the time we are fine with default constructor itself as other properties can be accessed and initialized
+	 * through getter setter methods.
+	 * */
+
+	/*
+	 * 2. No argument constructor
+	 * ---------------------------
+	 * Constructor without any argument is called no-args constructor. It’s like overriding the default constructor and
+	 * used to do some pre-initialization stuffs such as checking resources, network connections, logging etc.
+	 */
+
+	/**
+	 * No argument constructor - This will be called when we create a new instance of the class using the new keyword.
+	 */
+	private Introduction3() {
+		System.out.println("This is no argument constructor");
+	}
+
+	/*
+	 * 3. Parameterized constructor
+	 * -----------------------------
+	 * Constructor with arguments is called parameterized constructor.
+	 */
+	private Introduction3(String name) {
+		System.out.println("This is parameterized constructor");
+		// Initialize the name parameter. It will take the value of passed parameter.
+		this.name = name;
+		System.out.println("The name passsed as a parameter in the constructor is: " + name);
+	}
+
+	private void sampleMethod() {
+		System.out.println("This is the method that will be called by the instances of this class");
+		System.out.println("name: " + name);
+	}
+
+	public static void main(String[] args) {
+
+		// Creating an instance of the class using no-argument constructor
+		Introduction3 obj1 = new Introduction3();
+
+		// Creating an instance of the class using parameterized constructor
+		Introduction3 obj2 = new Introduction3("Anirudh Sharma");
+
+		// Calling the class method using the instances created above
+		obj1.sampleMethod();
+		obj2.sampleMethod();
+	}
+}

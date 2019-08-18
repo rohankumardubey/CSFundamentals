f93027944c4acb38e7dedc468c062f4c699a8c3e
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-08-18 19:41:09 +0530

    Added example of String repeat

---
+++ ProgrammingLanguages/Java/JavaFeatures/src/org/redquark/features/java11/strings/Repeat.java
@@ -0,0 +1,15 @@
+package org.redquark.features.java11.strings;
+
+/**
+ * @author Anirudh Sharma
+ *
+ */
+public class Repeat {
+
+	public static void main(String[] args) {
+
+		String str = "Anirudh";
+		System.out.println(str.repeat(5));
+	}
+
+}

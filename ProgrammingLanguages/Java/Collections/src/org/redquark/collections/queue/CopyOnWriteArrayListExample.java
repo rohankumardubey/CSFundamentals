46c2e1d55a9bdb6b49a150812ccde3b237a5078a
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-08-25 11:31:45 +0530

    Added example of CopyOnWriteArrayList

---
+++ ProgrammingLanguages/Java/Collections/src/org/redquark/collections/queue/CopyOnWriteArrayListExample.java
@@ -0,0 +1,37 @@
+package org.redquark.collections.queue;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author Anirudh Sharma
+ *
+ */
+public class CopyOnWriteArrayListExample {
+
+	public static void main(String[] args) {
+
+		// Creating CopyOnWriteArrayList and adding elements
+		List<Integer> cowal = new CopyOnWriteArrayList<>(new Integer[] { 1, 2, 3 });
+		System.out.println("Original list: " + cowal);
+
+		// Iterator 1
+		Iterator<Integer> iteratorOne = cowal.iterator();
+
+		// Add one more elements and verify if the list is updated
+		cowal.add(4);
+		System.out.println("Updated list: " + cowal);
+
+		// Get iterator 2
+		Iterator<Integer> iteratorTwo = cowal.iterator();
+
+		System.out.println("-----< Iterator 1 content >-----");
+		iteratorOne.forEachRemaining(System.out::println);
+
+		System.out.println("-----< Iterator 2 content >-----");
+		iteratorTwo.forEachRemaining(System.out::println);
+
+	}
+
+}

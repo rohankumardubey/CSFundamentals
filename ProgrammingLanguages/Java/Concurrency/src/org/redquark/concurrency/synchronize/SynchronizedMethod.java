c7f4182625590e3f552e9d990b549feee91a0938
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-08-27 12:09:37 +0530

    Added example of Synchronized method

---
+++ ProgrammingLanguages/Java/Concurrency/src/org/redquark/concurrency/synchronize/SynchronizedMethod.java
@@ -0,0 +1,37 @@
+package org.redquark.concurrency.synchronize;
+
+/**
+ * @author Anirudh Sharma
+ *
+ */
+public class SynchronizedMethod {
+
+	public static void main(String[] args) {
+
+		final Math math = new Math();
+
+		Runnable r = () -> {
+			math.printNumbers(3);
+		};
+
+		Thread threadOne = new Thread(r, "ONE");
+		Thread threadTwo = new Thread(r, "TWO");
+
+		threadOne.start();
+		threadTwo.start();
+	}
+
+	private static class Math {
+
+		public synchronized void printNumbers(int n) {
+			for (int i = 0; i < n; i++) {
+				try {
+					System.out.println(Thread.currentThread().getName() + " :: " + i);
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}

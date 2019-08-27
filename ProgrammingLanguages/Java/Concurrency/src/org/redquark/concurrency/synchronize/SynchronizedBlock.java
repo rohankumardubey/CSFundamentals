77ca007b5996a281cba425394a7e8e581aacb9a2
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-08-27 12:03:13 +0530

    Added example of Synchronized block

---
+++ ProgrammingLanguages/Java/Concurrency/src/org/redquark/concurrency/synchronize/SynchronizedBlock.java
@@ -0,0 +1,40 @@
+package org.redquark.concurrency.synchronize;
+
+/**
+ * @author Anirudh Sharma
+ *
+ */
+public class SynchronizedBlock {
+
+	public static void main(String[] args) {
+
+		final Math math = new Math();
+		
+		Runnable r = () -> {
+			math.printNumbers(3);
+		};
+		
+		Thread threadOne = new Thread(r, "ONE");
+		Thread threadTwo = new Thread(r, "TWO");
+		
+		threadOne.start();
+		threadTwo.start();
+	}
+
+	private static class Math {
+
+		public void printNumbers(int n) {
+
+			try {
+				synchronized (this) {
+					for (int i = 0; i < n; i++) {
+						System.out.println(Thread.currentThread().getName() + " :: " + i);
+						Thread.sleep(500);
+					}
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}

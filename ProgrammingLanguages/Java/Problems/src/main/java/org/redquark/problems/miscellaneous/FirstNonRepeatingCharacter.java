a24a46eec6218b44665e119792396551affc62be
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-10-14 09:32:08 +0530

    Updated the code for first non repeating character to make it more optimized

--- ProgrammingLanguages/Java/Problems/src/main/java/org/redquark/problems/miscellaneous/FirstNonRepeatingCharacter.java
+++ ProgrammingLanguages/Java/Problems/src/main/java/org/redquark/problems/miscellaneous/FirstNonRepeatingCharacter.java
@@ -1,8 +1,14 @@
 package org.redquark.problems.miscellaneous;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
+ * Given a string, find the first non-repeating character in it. For example, if
+ * the input string is “GeeksforGeeks”, then output should be ‘f’ and if input
+ * string is “GeeksQuiz”, then output should be ‘G’.
+ * 
  * @author Anirudh Sharma
  *
  */
@@ -19,18 +25,45 @@ public class FirstNonRepeatingCharacter {
 		// Closing the Scanner to avoid memory leakage
 		in.close();
 
-		// Converting the String to lower case
-		s = s.toLowerCase();
+		System.out.println(
+				"First non repeating character in the string: " + s + " is => " + getFirstNonRepeatingCharacter(s));
+	}
 
-		// Loop for the length of the String for each character
-		for (Character c : s.toCharArray()) {
-			// If the first index of the character is also the last index of that character
-			// then it means this character is not repeating
-			if (s.indexOf(c) == s.lastIndexOf(c)) {
-				// Print the character and break the loop
-				System.out.println(s.indexOf(c));
-				break;
+	/**
+	 * This method returns the first non-repeating character in the given String
+	 */
+	private static char getFirstNonRepeatingCharacter(String s) {
+
+		// Map to store the character and its frequency in the given String
+		Map<Character, Integer> countMap = new LinkedHashMap<>();
+
+		// Loop through the entire String character by character
+		for (int i = 0; i < s.length(); i++) {
+			// Find the current character
+			char c = s.charAt(i);
+
+			// Check if the character is already present in the Map
+			if (countMap.containsKey(c)) {
+				// Get the current count of 'c'
+				int currentCount = countMap.get(c);
+				// Increment the current count by 1 and update the map
+				countMap.put(c, currentCount + 1);
+			} else {
+				// If the current character is encountered for the first time
+				countMap.put(c, 1);
 			}
 		}
+
+		// Loop through the entire map to find the first character whose count is 1
+		for (Map.Entry<Character, Integer> entry : countMap.entrySet()) {
+			// Check if the value of current entry object is 1
+			if (entry.getValue() == 1) {
+				// Return the corresponding key
+				return entry.getKey();
+			}
+		}
+
+		// If all the characters are repeating then return -1
+		return '\u0000';
 	}
 }

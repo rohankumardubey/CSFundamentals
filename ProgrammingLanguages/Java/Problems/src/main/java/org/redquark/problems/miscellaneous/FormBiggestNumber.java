65606729980449683f0c0f477011e0220cdde4dc
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-10-14 09:46:57 +0530

    Updated the logic for finding the biggest element from the array items

--- ProgrammingLanguages/Java/Problems/src/main/java/org/redquark/problems/miscellaneous/FormBiggestNumber.java
+++ ProgrammingLanguages/Java/Problems/src/main/java/org/redquark/problems/miscellaneous/FormBiggestNumber.java
@@ -1,11 +1,17 @@
 package org.redquark.problems.miscellaneous;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 /**
+ * Given an array of numbers, arrange them in a way that yields the largest
+ * value. For example, if the given numbers are {54, 546, 548, 60}, the
+ * arrangement 6054854654 gives the largest value. And if the given numbers are
+ * {1, 34, 3, 98, 9, 76, 45, 4}, then the arrangement 998764543431 gives the
+ * largest value.
+ * 
  * @author Anirudh Sharma
  *
  */
@@ -13,14 +19,18 @@ public class FormBiggestNumber {
 
 	public static void main(String[] args) {
 
-		// List to store numbers
-		List<String> numbers = Arrays.asList("01", "34", "3", "98", "9", "76", "45", "4");
-		
-		for (int i = 0; i < numbers.size(); i++) {
-			final Integer intS = Integer.parseInt(numbers.get(i));
-			numbers.set(i, intS.toString());
+		// Input array of numbers
+		int[] input = { 01, 34, 3, 98, 9, 76, 45, 4 };
+
+		// List that will store the integer representation of numbers
+		List<String> numbers = new ArrayList<>(input.length);
+
+		// Add all the integers from input array to the List of String
+		for (int i = 0; i < input.length; i++) {
+			numbers.add(Integer.toString(input[i]));
 		}
 
+		// Sort the elements in the list using custom comparator
 		Collections.sort(numbers, new Comparator<String>() {
 
 			@Override
@@ -35,16 +45,15 @@ public class FormBiggestNumber {
 			}
 		});
 
+		// This StringBuilder instance will store the final output
 		StringBuilder sb = new StringBuilder();
-        for(String s : numbers) {
-            sb.append(s);
-        }
-        String result = sb.toString();
-        if(Long.parseLong(result) == 0) {
-            result = "0";
-        }
-        
-        System.out.println(result);
+		// Loop through each element of the List
+		for (String s : numbers) {
+			// Append the current element
+			sb.append(s);
+		}
+
+		System.out.println(sb.toString());
 	}
 
 }
